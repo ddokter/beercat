eef2f98e7b0cdd791725252659247d92bba326e2
Duco Dokter <d@etcanemtuum.org>
2020-05-16 17:16:33 +0200

    added rudimentary search

--- beercat/views/search.py
+++ beercat/views/search.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
+# from django.contrib.postgres.search import SearchVector
+from django.db.models import Q
+from ..models.brewery import Brewery
+from ..models.style import Style
+from ..models.person import Person
 
 
 class Search(TemplateView):
@@ -7,4 +12,21 @@ class Search(TemplateView):
 
     def list_items(self):
 
-        return []
+        results = []
+
+        if self.request.GET.get('q'):
+
+            query = self.request.GET.get('q')
+
+            for brewery in Brewery.objects.filter(name__icontains=query):
+                results.append(brewery)
+
+            for person in Person.objects.filter(
+                    Q(name__icontains=query) | Q(surname__icontains=query)):
+                results.append(person)
+
+            for style in Style.objects.filter(
+                    Q(name__icontains=query) | Q(synonyms__icontains=query)):
+                results.append(style)
+
+        return results
